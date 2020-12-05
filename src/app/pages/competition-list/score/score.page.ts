99644b9d67c6a349bf1729c81d86bf72b784b5c6
anuwat456 <anuwat_456@yahoo.com>
2020-12-05 18:13:58 +0700

    create score page complete macth

--- src/app/pages/competition-list/score/score.page.ts
+++ src/app/pages/competition-list/score/score.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-score',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScorePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private alertController: AlertController,
+  ) { }
 
   ngOnInit() {
   }
 
+  async completeMacth() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'ยืนยันผลการแข่งขัน',
+      buttons: [
+        {
+          text: 'ยกเลิก',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'ยืนยัน',
+          handler: () => {
+            console.log('Confirm Okay');
+            this.router.navigate(['../../competition-list']);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
